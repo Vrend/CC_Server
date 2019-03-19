6db7e3274793b9238e495afe6913023d1d963866
John Broderick <johnbroderick12@gmail.com>
2019-03-19 14:52:12 -0400

    Updated for GNU GPLv3 Compliance

--- local_functions.cpp
+++ local_functions.cpp
@@ -1,3 +1,20 @@
+/*
+This file is part of CC_Server.
+
+CC_Server is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+CC_Server is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+long with CC_Server.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include "local_functions.h"
 
 //Confirms that given IP and Port are valid
